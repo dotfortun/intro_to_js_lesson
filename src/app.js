e537855ab7805418465b3ded55e014d11e2f7eb7
dotfortun <being.for.itself@gmail.com>
2022-02-16 22:58:03 +0000

    Getting the structure set up.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getElementUnderMouse() {
+  let elems = [];
+  document.querySelectorAll(":hover").forEach(el => {
+    elems.push(el.id);
+  });
+  return elems;
+}
+
+function look(target) {
+  // Unimplemented
+}
+
+function take(target) {
+  // Unimplemented
+}
+
+function use(target) {
+  // Unimplemented
+}
+
+function speak(target) {
+  // Unimplemented
+}
+
+function turn(direction) {
+  // Unimplemented
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // setInterval(() => {
+  //   console.log(getElementUnderMouse());
+  // }, 1000);
 };
---
+++ src/room.js
@@ -0,0 +1,22 @@
+export default roomData = {
+  objects: {
+    computer: {
+      look: "It's a computer with that flying toaster screensaver.",
+      take: "That computer won't fit in your inventory.",
+      use:
+        "The computer is password protected (and isn't showing the toasters anymore.) :(",
+      speak: "The computer seems indifferent to your thoughts.",
+    },
+    books: {
+        look: "Looks like this person is a nerd, they've got 3 programming books and 'The Visual Display Of Quantitative Information', whatever that is.",
+        take: "If you want to take books, visit the library.",
+        use: () => {
+            roomData.objects.computer.use = () => {
+                // Todo.
+                return "The computer seems to be able to control the doors?"
+            }
+        },
+        speak: "The computer seems indifferent to your thoughts.",
+      },
+  },
+};
