0e48cd89208d995bea454da6c936355b1272d10a
dotfortun <being.for.itself@gmail.com>
2022-02-22 17:45:28 +0000

    And that will do it for now.

--- src/room.js
+++ src/room.js
@@ -1,11 +1,17 @@
 let roomData = {
   objects: {
     computer: {
-      look: "It's a computer with that flying toaster screensaver.",
+      look: args => {
+        if (roomData.note.hasBeenTaken) {
+          return "It's a computer with that flying toaster screensaver.";
+        } else {
+          return "Oh, the toasters are gone.";
+        }
+      },
       take: "That computer won't fit in your inventory.",
       use: div => {
         if (roomData.note.hasBeenTaken) {
-          div.styles.background = `url("../src/assets/img/office/desk_01.png")`;
+          div.style.backgroundImage = `url("../src/assets/img/office/desk_02.png")`;
           return "The computer is no longer password protected (and isn't showing the toasters anymore.) :(";
         }
         return "The computer is password protected. :(";
@@ -20,18 +26,17 @@ let roomData = {
         "You pick up the handset and talk to the nice dialtone for a while."
     },
     fruit: {
-      look: "Wait, people still use landlines?",
-      take: "That phone definitely doesn't belong to you.",
-      use: "You pick up the handset and talk to the nice dialtone for a while.",
-      speak:
-        "You pick up the handset and talk to the nice dialtone for a while."
+      look: "Looks delicious and low-poly.",
+      take: "Every time you take a piece of fruit, another replaces it.",
+      use: "You chew on some fruit.  It tastes of vertices.",
+      speak: "You pick up the bana and talk to the nice fruit for a while."
     },
     poster: {
       look: "This person obviously wants a better job.",
-      take: "That phone definitely doesn't belong to you.",
-      use: "You pick up the handset and talk to the nice dialtone for a while.",
+      take: "Well that's rude.",
+      use: "You appreciate the poster for a while.",
       speak:
-        "You pick up the handset and talk to the nice dialtone for a while."
+        "The poster doesn't seems to have any opinion about what you say to it."
     },
     note: {
       hasBeenTaken: false,
@@ -44,9 +49,8 @@ let roomData = {
           return "Wait, is that a password?";
         }
       },
-      use: "You pick up the handset and talk to the nice dialtone for a while.",
-      speak:
-        "You pick up the handset and talk to the nice dialtone for a while."
+      use: "You add a little doodle to the note.",
+      speak: "The note fails to transcribe your words."
     }
   }
 };
